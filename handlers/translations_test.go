c6c51b7bdb3bfd65a914fae8613625322390608b
KrumK <krum.kereziev@mentormate.com>
2021-09-21 00:20:23 +0300

    [Add]: integration tests for the endpoints

--- handlers/translations_test.go
+++ handlers/translations_test.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -19,11 +18,10 @@ func init() {
 }
 
 func TestTranslateWord(t *testing.T) {
-
-	req := httptest.NewRequest(http.MethodPost, "/word", strings.NewReader(`{"english_word": "apple"}`))
+	r := httptest.NewRequest(http.MethodPost, "/word", strings.NewReader(`{"english_word": "apple"}`))
 	w := httptest.NewRecorder()
 
-	th.TranslateWord(w, req)
+	th.TranslateWord(w, r)
 
 	if w.Code != http.StatusOK {
 		t.Errorf("Word translation didn't return %v", http.StatusOK)
@@ -34,10 +32,52 @@ func TestTranslateWord(t *testing.T) {
 	response, err := ioutil.ReadAll(w.Body)
 
 	if err != nil {
-		t.Errorf("reading response body: %v, want %v", err, expected)
+		t.Errorf("Reading response body: %v, want %v", err, expected)
 	}
 
-  fmt.Println()
+	if !strings.Contains(string(response), expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			string(response), expected)
+	}
+}
+
+func TestTranslateSentence(t *testing.T) {
+	r := httptest.NewRequest(http.MethodPost, "/sentence", strings.NewReader(`{"english_sentence": "absolutely marvalous!"}`))
+	w := httptest.NewRecorder()
+
+	th.TranslateSentence(w, r)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Sentence translation didn't return %v", http.StatusOK)
+	}
+
+	expected := `{"gopher_sentence":"gabsolutely arvalous!"}`
+
+	response, err := ioutil.ReadAll(w.Body)
+
+	if err != nil {
+		t.Errorf("Reading response body: %v, want %v", err, expected)
+	}
+
+	if !strings.Contains(string(response), expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			string(response), expected)
+	}
+}
+
+func TestGetHistory(t *testing.T) {
+	r := httptest.NewRequest(http.MethodGet, "/history", nil)
+	w := httptest.NewRecorder()
+	th.GetHistory(w, r)
+
+  // already in the db due to our previous two tests
+	expected := `{"history":[{"absolutely marvalous!":"gabsolutely arvalous!"},{"apple":"gapple"}]}`
+	response, err := ioutil.ReadAll(w.Body)
+
+	if err != nil {
+		t.Errorf("Reading response body: %v, want %v", err, "")
+	}
+
 	if !strings.Contains(string(response), expected) {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			string(response), expected)
